17d211e88d3f52ed465d2c48cb3271e22789cdf1
bielzox <leogabriel2662@gmail.com>
2026-09-17 19:00:17 -0300

    troca transporter

--- backend/server.js
+++ backend/server.js
@@ -17,10 +17,15 @@ const IV_LENGTH = 16;
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASSWORD
+    },
+    tls: {
+        rejectUnauthorized: false
     }
 });
 
