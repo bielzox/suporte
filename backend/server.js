3e1aed677f083bc1d5634d7459c9628036dcad45
leonardo <leogabriel2662@gmail.com>
2026-09-17 15:49:04 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -209,6 +209,9 @@ const transporter = nodemailer.createTransport({
   port: 465,
   secure: true,
   family: 4,
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
