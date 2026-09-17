6c5f93f828bc602695e9f9a6dd43c01e1bd5949c
leonardo <leogabriel2662@gmail.com>
2026-09-17 15:42:50 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -205,16 +205,14 @@ const db = {
 // ============================================================
 
 const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASSWORD
-    },
-    tls: {
-        rejectUnauthorized: false
-    }
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  family: 4,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  }
 });
 
 // ============================================================
